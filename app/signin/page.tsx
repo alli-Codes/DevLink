bff776efb34c3bb24fb6267695fdfdf97ad75920
= <=>
2024-07-23 23:43:54 +0100

    feat: make h-screen, remove styles in css file

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,3 +1,23 @@
 export default function SignIn() {
-    return <div>Heu</div>
+    return <div className="h-screen flex flex-col">
+        <section>Devlink</section>
+        <section className="flex flex-col gap-10">
+            <div className="flex flex-col gap-2">
+                <h1>Login</h1>
+                <p>Add your details below to get back into the app</p>
+            </div>
+
+            <div className="flex flex-col gap-6">
+                <section className="flex flex-col gap-3">
+                    <label htmlFor="email">Email address</label>
+                <input className="px-4 py-3" type="email" name="" id="" />
+                </section>
+                <section className="flex flex-col gap-3">
+                    <label htmlFor="email">Password</label>
+                <input type="email" name="" id="" />
+                </section>
+                <button>Login</button>
+            </div>
+        </section>
+    </div>
 }
