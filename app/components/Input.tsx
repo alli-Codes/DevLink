347946e18d84b18b2ebc7be9e4876af699914f8b
= <=>
2024-07-24 23:54:20 +0100

    fix: fix input component

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -14,7 +14,7 @@ export default function Input({ type, placeholder, label, showLabel, onValueChan
     return (
         <div className="flex flex-col gap-1">
             {showLabel ? <label htmlFor="" className="text-[#333333] text-xs ">{ label }</label> : undefined}
-            <input className="px-4 py-3 border border-[#D9D9D9] rounded-lg" type={type} placeholder={placeholder} name="" id="" onChange={(e) => handleInputChange(e.target.value)} />
+            <input className="px-4 py-3 border border-[#D9D9D9] rounded-lg" type={type} placeholder={placeholder} name="" id="" />
         </div>
     )
 }
