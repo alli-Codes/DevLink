f8262209e9d24a90d71da553d868a46c2af4093a
= <=>
2024-07-24 07:44:50 +0100

    feat: create components folder, and Input component

---
+++ app/components/Input.tsx
@@ -0,0 +1,7 @@
+export default function Input({ type, placeholder }: { type: string, placeholder: string }) {
+    return (
+        <div>
+            <input className="px-4 py-3 border border-[#D9D9D9] rounded-lg" type={type} placeholder={placeholder} name="" id="" />
+        </div>
+    )
+}
