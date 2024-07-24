f965f9781c265a06492e5dfdf41c28ab1b1a1e02
= <=>
2024-07-25 00:55:29 +0100

    feat: make link and profile navigation work

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,14 +2,15 @@ import Image from "next/image"
 import LinkIcon from "./LinkIcon"
 import ProfileIcon from "./ProfileIcon"
 import EyeIcon from "./EyeIcon"
+import Link from "next/link"
 
 export default function Header() {
     return (
         <header className="bg-white py-4 px-6 flex items-center justify-between sticky top-0">
             <Image  src="/logo.png" width={26} height={26} alt="" />
             <div className="flex gap-2">
-                <LinkIcon />
-                <ProfileIcon />
+                <Link href="/dashboard/createNewLink"><LinkIcon /></Link>
+                <Link href="/dashboard/profileInfo"><ProfileIcon /></Link>
             </div>
             <EyeIcon />
         </header>
