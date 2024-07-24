352c427d02c8cc82ed4b71a2cabcecc629cce510
= <=>
2024-07-24 08:43:49 +0100

    feat: add labels inside the input components, pass props to show or hide labels

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,5 +1,8 @@
-export default function Input({ type, placeholder }: { type: string, placeholder: string }) {
+export default function Input({ type, placeholder, label, showLabel }: { type: string, placeholder: string, label: string, showLabel: boolean }) {
     return (
+        <div className="flex flex-col gap-1">
+            {showLabel ? <label htmlFor="" className="text-[#333333] text-xs ">{ label }</label> : undefined}
             <input className="px-4 py-3 border border-[#D9D9D9] rounded-lg" type={type} placeholder={placeholder} name="" id="" />
+        </div>
     )
 }
