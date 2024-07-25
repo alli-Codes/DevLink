05539b931c4d2d0be6db3499b9a1f4b336fa5272
= <=>
2024-07-25 09:17:01 +0100

    feat: include icons

--- app/components/LinkInput.tsx
+++ app/components/LinkInput.tsx
@@ -1,9 +1,14 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 export default function LinkInput() {
     return (
         <div className="flex flex-col gap-1">
             <label htmlFor="" className="text-[#333333] text-xs ">Link</label>
             {/* <div> */}
-                <input className="bg-white w-full px-4 py-3 border border-[#D9D9D9] rounded-lg" />
+            <div className="bg-white w-full px-4 py-3 flex items-center border border-[#D9D9D9] rounded-lg">
+                <Icon icon="ph:link-bold" />
+                <input type="text" />
+            </div>
             {/* </div> */}
         </div>
     )
--- app/components/LinkPlatform.tsx
+++ app/components/LinkPlatform.tsx
@@ -1,9 +1,13 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 export default function LinkPlatform() {
     return (
         <div className="flex flex-col gap-1">
              <label htmlFor="" className="text-[#333333] text-xs ">Platform</label>
-            <div>
-                <p className="bg-white px-4 py-3 border border-[#D9D9D9] rounded-lg">GitHub</p>
+                <div className="bg-white px-4 py-3 flex items-center gap-3 border border-[#D9D9D9] rounded-lg">
+                    <Icon icon="fa6-brands:github" />
+                    <p className="w-full">GitHub</p>
+                    <Icon icon="mdi:chevron-down" color="#633CFF"/>
             </div>
         </div>
     )
